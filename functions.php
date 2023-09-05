6f56bb5fdf0e96350d8075dc66cc3280f1307d80
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2023-09-05 15:29:43 -0700

    add title tag in functions.php

--- functions.php
+++ functions.php
@@ -20,3 +20,10 @@ function add_theme_scripts(){
 
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+function title_setup(){
+    add_theme_support('title-tag');
+    add_theme_support('automatic-feed-links');
+
+}
+add_action('after_setup_theme' , 'title_setup');
--- header.php
+++ header.php
@@ -7,13 +7,12 @@
 
     <!--meta-->
     <meta name="description" content="Quiety creative Saas, software technology, Saas agency & business Bootstrap 5 Html template. It is best and famous software company and Saas website template.">
-    <meta name="author" content="ThemeTags">
+    <meta name="author" content="">
 
     <!--favicon icon-->
     <link rel="icon" href="<?php echo get_template_directory_uri()?>/assets/img/favicon.png" type="image/png" sizes="16x16">
 
-    <!--title-->
-    <title> Software & IT Solutions HTML Template</title>
+
 
     <!--google fonts-->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&amp;family=Open+Sans:wght@400;600&amp;display=swap" rel="stylesheet">
